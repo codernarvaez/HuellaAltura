5c7a59a0f2adc1ac97484f221be2dd61e298195d
Gerardo Herrera <gerardo.herrera@unl.edu.ec>
2026-06-14 21:14:44 -0500

    actualizacion de rama y creacion de servicio login para comunicacion con backend

--- HAFront/src/services/auth.service.ts
+++ HAFront/src/services/auth.service.ts
@@ -36,7 +36,7 @@ export interface UserOut {
   updated_at?: string;
 }
 
-//agregamos las interfaces para la recuperación de contra
+//agregamos las interfaces para la recuperación de contraaase
 
 export interface PasswordResetRequest {
   email: string;
